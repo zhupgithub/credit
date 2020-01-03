d21ecc2c167fd762c34fb61597c8c2728e53df47
zhupgithub <1539543015@qq.com>
2020-01-03 15:00:04 +0800

    配置中心

---
+++ master/pc/admin2/src/main/java/com/zhupeng/AdminApplication.java
@@ -0,0 +1,32 @@
+package com.zhupeng;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Slf4j
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableAdminServer
+public class AdminApplication {
+
+    public static void main(String[] args) {
+        log.info("admin监控启动中……");
+        SpringApplication.run(AdminApplication.class , args);
+        log.info("admin监控启动完成！");
+    }
+    @Configuration
+    public class SecurityPermitAllConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().permitAll()
+                    .and().csrf().disable();
+        }
+    }
+}
